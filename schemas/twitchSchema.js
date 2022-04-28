137e119675f0d5501ae0424fe74dc315b9374692
franciscososanet <fransosa.dev@gmail.com>
2022-04-28 17:35:31 -0300

    Arreglos previos a musica

--- schemas/twitchSchema.js
+++ schemas/twitchSchema.js
@@ -2,15 +2,8 @@ const { Schema, model } = require("mongoose");
 
 const twitch = new Schema({
 
-    user: {
-        type: String,
-        required: true
-    },
-
-    titulo: {
-        type: String,
-        required: true
-    }
+    user: { type: String, required: true },
+    titulo: { type: String, required: true }
 });
 
 module.exports = model("twitchSchema", twitch);
